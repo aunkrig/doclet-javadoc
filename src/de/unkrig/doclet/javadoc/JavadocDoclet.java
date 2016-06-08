a227b5be68bc618942ab8d50ae72b332c808640d
Arno Unkrig <aunkrig@users.noreply.github.com>
2016-06-08 11:05:21 +0000

    Refactored the generation of the index pages: Now using "IndexPages" instead of a template.

--- src/de/unkrig/doclet/javadoc/JavadocDoclet.java
+++ src/de/unkrig/doclet/javadoc/JavadocDoclet.java
@@ -28,21 +28,29 @@ package de.unkrig.doclet.javadoc;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 import com.sun.javadoc.ClassDoc;
+import com.sun.javadoc.Doc;
 import com.sun.javadoc.LanguageVersion;
+import com.sun.javadoc.MethodDoc;
 import com.sun.javadoc.PackageDoc;
+import com.sun.javadoc.ProgramElementDoc;
 import com.sun.javadoc.RootDoc;
 
 import de.unkrig.commons.doclet.Docs;
+import de.unkrig.commons.doclet.html.Html;
 import de.unkrig.commons.lang.AssertionUtil;
 import de.unkrig.commons.lang.StringUtil;
 import de.unkrig.commons.lang.protocol.ConsumerWhichThrows;
+import de.unkrig.commons.lang.protocol.Longjump;
 import de.unkrig.commons.util.collections.IterableUtil;
 import de.unkrig.commons.util.collections.IterableUtil.ElementWithContext;
+import de.unkrig.doclet.javadoc.templates.JavadocUtil;
 import de.unkrig.doclet.javadoc.templates.clasS.ClassDetailHtml;
 import de.unkrig.doclet.javadoc.templates.clasS.PerClassDocument;
 import de.unkrig.doclet.javadoc.templates.global.AllclassesFrameHtml;
@@ -51,7 +59,6 @@ import de.unkrig.doclet.javadoc.templates.global.ConstantValuesHtml;
 import de.unkrig.doclet.javadoc.templates.global.DeprecatedListHtml;
 import de.unkrig.doclet.javadoc.templates.global.GlobalDocument;
 import de.unkrig.doclet.javadoc.templates.global.HelpDocHtml;
-import de.unkrig.doclet.javadoc.templates.global.IndexAllHtml;
 import de.unkrig.doclet.javadoc.templates.global.IndexHtml;
 import de.unkrig.doclet.javadoc.templates.global.OverviewFrameHtml;
 import de.unkrig.doclet.javadoc.templates.global.OverviewSummaryHtml;
@@ -64,7 +71,10 @@ import de.unkrig.doclet.javadoc.templates.packagE.PackageSummaryHtml;
 import de.unkrig.doclet.javadoc.templates.packagE.PackageTreeHtml;
 import de.unkrig.doclet.javadoc.templates.packagE.PerPackageDocument;
 import de.unkrig.notemplate.NoTemplate;
+import de.unkrig.notemplate.javadocish.IndexPages;
+import de.unkrig.notemplate.javadocish.IndexPages.IndexEntry;
 import de.unkrig.notemplate.javadocish.Options;
+import de.unkrig.notemplate.javadocish.templates.AbstractRightFrameHtml;
 
 /**
  * A doclet that generates documentation for Java packages, classes, and so forth.
@@ -94,6 +104,38 @@ class JavadocDoclet {
         if ("-top".equals(option))         return 2;
         if ("-bottom".equals(option))      return 2;
         if ("-notimestamp".equals(option)) return 1;
+        if ("-splitindex".equals(option))  return 1;
+
+        // Options of the JAVADOC standard doclet that are NOT (yet) implemented by this doclet:
+        //    -use
+        //    -version
+        //    -author
+        //    -title title
+        //    -link extdocURL
+        //    -linkoffline extdocURL packagelistLoc
+        //    -linksource
+        //    -nodeprecated
+        //    -nodeprecatedlist
+        //    -nosince
+        //    -notree
+        //    -noindex
+        //    -nohelp
+        //    -nonavbar
+        //    -helpfile file
+        //    -stylesheetfile file
+        //    -serialwarn
+        //    -charset name
+        //    -docencoding name
+        //    -keywords
+        //    -tag tagname:Xaoptcmf:"taghead"
+        //    -taglet class
+        //    -tagletpath tagletpathlist
+        //    -docfilessubdirs
+        //    -excludedocfilessubdir name1:name2...
+        //    -noqualifier all | packagename1:packagename2:...
+        //    -nocomment
+        //    -sourcetab tabLength
+        // See http://docs.oracle.com/javase/7/docs/technotes/tools/windows/javadoc.html#standard
 
         return 0;
     }
@@ -118,6 +160,7 @@ class JavadocDoclet {
             if ("-top".equals(option[0]))         { options.top         = option[1];           } else
             if ("-bottom".equals(option[0]))      { options.bottom      = option[1];           } else
             if ("-notimestamp".equals(option[0])) { options.noTimestamp = true;                } else
+            if ("-splitindex".equals(option[0]))  { options.splitIndex  = true;                } else
             {
 
                 // It is quite counterintuitive, but 'options()' returns ALL options, not only those which
@@ -170,7 +213,6 @@ class JavadocDoclet {
         this.generateGlobalDocument(OverviewTreeHtml.class,      "overview-tree.html");
         this.generateGlobalDocument(DeprecatedListHtml.class,    "deprecated-list.html");
         this.generateGlobalDocument(HelpDocHtml.class,           "help-doc.html");
-        this.generateGlobalDocument(IndexAllHtml.class,          "index-all.html");
         this.generateGlobalDocument(PackageList.class,           "package-list");
         this.generateGlobalDocument(SerializedFormHtml.class,    "serialized-form.html");
 
@@ -188,6 +230,73 @@ class JavadocDoclet {
                 // Create per-class document.
                 this.generatePerClassDocument(clasS, ClassDetailHtml.class);
             }
+        }
+
+        // Generate the (alphabetical) index document(s).
+        {
+            Collection<Doc> allDocs = new ArrayList<Doc>();
+
+            for (PackageDoc pd : this.allPackages) {
+                allDocs.add(pd);
+            }
+
+            for (ClassDoc cd : this.allClassesAndInterfaces) {
+                for (MethodDoc md : cd.methods()) {
+                    allDocs.add(md);
+                }
+            }
+
+            Collection<IndexEntry> indexEntries = new ArrayList<>();
+
+            for (Doc doc : allDocs) {
+                indexEntries.add(new IndexEntry() {
+
+                    @Override public String
+                    getShortDescription() {
+                        return JavadocUtil.firstSentenceOfDescription(
+                            doc instanceof MethodDoc ? ((ProgramElementDoc) doc).containingClass() : doc,  // from
+                            doc,                                                                           // to
+                            JavadocDoclet.this.rootDoc
+                        );
+                    }
+
+                    @Override public String
+                    getLink() {
+                        return AssertionUtil.notNull(Longjump.catchLongjump(() -> Html.STANDARD_LINK_MAKER.makeLink(
+                            JavadocDoclet.this.rootDoc,
+                            doc,
+                            JavadocDoclet.this.rootDoc
+                        ).href, "???"));
+                    }
+
+                    @Override public String
+                    getKey() {
+                        return AssertionUtil.notNull(Longjump.catchLongjump(() -> Html.STANDARD_LINK_MAKER.makeLink(
+                            JavadocDoclet.this.rootDoc,
+                            doc,
+                            JavadocDoclet.this.rootDoc
+                        ).defaultLabelHtml, "???"));
+                    }
+
+                    @Override public String
+                    getExplanation() { return ""; }
+                });
+            }
+
+            IndexPages.createIndex(
+                this.options.destination, // outputFile
+                indexEntries,             // indexEntries
+                this.options,             // options
+                new String[] {            // nav1
+                    "Overview",   "./overview-summary.html",
+                    "Package",    AbstractRightFrameHtml.DISABLED,
+                    "Class",      AbstractRightFrameHtml.DISABLED,
+                    "Tree",       "./overview-tree.html",
+                    "Deprecated", "./deprecated-list.html",
+                    "Index",      AbstractRightFrameHtml.HIGHLIT,
+                    "Help",       "./help-doc.html",
+                }
+            );
         }
     }
 
